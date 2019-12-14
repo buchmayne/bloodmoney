35c1e49169da12031f143ad46119c2eaa6ca19f0
buchmayne <michaelmbuchman@gmail.com>
2019-12-14 13:25:11 -0800

    commented out unnecessary functions and parameters

--- scrapers/models.py
+++ scrapers/models.py
@@ -1,19 +1,19 @@
-from sqlalchemy import create_engine, Column, Integer, String, DateTime
+from sqlalchemy import create_engine
 from sqlalchemy.engine.url import URL
-from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.ext.declarative import declarative_base
 
 import settings
 
 # http://newcoder.io/scrape/part-4/
 
-DeclarativeBase = declarative_base()
+# DeclarativeBase = declarative_base()
 
 
-def create_eventid_table(engine):
-    '''
-    ?
-    '''
-    DeclarativeBase.metadata.create_all(engine)
+# def create_eventid_table(engine):
+#     '''
+#     ?
+#     '''
+#     DeclarativeBase.metadata.create_all(engine)
 
 
 def db_connect():
@@ -24,29 +24,29 @@ def db_connect():
     return create_engine(URL(**settings.DATABASE))
 
 
-class EventId(DeclarativeBase):
-    '''
-    Table for Fight URL extensions and EventIds
-    '''
-    __tablename__ = "eventid"
+# class EventId(DeclarativeBase):
+#     '''
+#     Table for Fight URL extensions and EventIds
+#     '''
+#     __tablename__ = "eventid"
 
-    id = Column(Integer, primary_key=True)
-    event_url = Column('event_url', String, nullable=True)
-    event_id = Column('event_id', String, nullable=True)
+#     id = Column(Integer, primary_key=True)
+#     event_url = Column('event_url', String, nullable=True)
+#     event_id = Column('event_id', String, nullable=True)
 
 
-class Events(DeclarativeBase):
-    """Events Data Model"""
-    __tablename__ = "events"
+# class Events(DeclarativeBase):
+#     """Events Data Model"""
+#     __tablename__ = "events"
 
-    id = Column(Integer, primary_key=True)
-    event_id = Column('event_id', String, nullable=True)
-    timestamp = Column('timestamp', DateTime, nullable=True)
-    date = Column('date', String, nullable=True)
-    gmt = Column('gmt', String, nullable=True)
-    venue = Column('venue', String, nullable=True)
-    country = Column('country', String, nullable=True)
-    city = Column('city', String, nullable=True)
-    cur_fight = Column('cur_fight', String, nullable=True)
-    event_start = Column('event_start', String, nullable=True)
-    event_end = Column('event_end', String, nullable=True)
+#     id = Column(Integer, primary_key=True)
+#     event_id = Column('event_id', String, nullable=True)
+#     timestamp = Column('timestamp', DateTime, nullable=True)
+#     date = Column('date', String, nullable=True)
+#     gmt = Column('gmt', String, nullable=True)
+#     venue = Column('venue', String, nullable=True)
+#     country = Column('country', String, nullable=True)
+#     city = Column('city', String, nullable=True)
+#     cur_fight = Column('cur_fight', String, nullable=True)
+#     event_start = Column('event_start', String, nullable=True)
+#     event_end = Column('event_end', String, nullable=True)
