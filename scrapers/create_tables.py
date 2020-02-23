05abd2ed7beceaa89a7fad3d6fd1f29bb803aa08
buchmayne <michaelmbuchman@gmail.com>
2020-02-23 11:59:53 -0800

    dropped GMT from event_data table

--- scrapers/create_tables.py
+++ scrapers/create_tables.py
@@ -9,7 +9,6 @@ def create_tables():
             event_id INTEGER PRIMARY KEY,
             time_stamp TIMESTAMP,
             date DATE,
-            gmt VARCHAR (70),
             venue VARCHAR (70),
             country VARCHAR (70),
             city VARCHAR (70),
