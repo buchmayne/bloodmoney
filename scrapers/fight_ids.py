9d71b4af158546ce813a7dd2a31087cecf3ace9b
buchmayne <michaelmbuchman@gmail.com>
2020-02-29 13:44:59 -0800

    need to handle situations where the most recent id is not on the first page of past events. added a note to account for where the change needs to be made

--- scrapers/fight_ids.py
+++ scrapers/fight_ids.py
@@ -1,40 +1,9 @@
 import re
-import math
 from requests import get
 import pandas as pd
 from bs4 import BeautifulSoup
 import psycopg2
-from parameters import landing_page, headers, events_per_page, events_page
-
-
-def get_total_pages(events_per_page=events_per_page, landing_page=landing_page, headers=headers):
-    """
-    Inputs:
-        events_per_page: int
-            the number of events on each page of the ufc site
-        landing_page: str
-            landing page is mostly likely static and directs to https://www.ufc.com/events
-        headers: dict
-            headers defines the user agent and is used in the get request
-    Outputs:
-        total_pages: int
-            integer count of the total past ufc events divided by events per page
-    """
-    page = get(landing_page, headers=headers)
-    page_html = BeautifulSoup(page.content, 'html.parser')
-
-    past_events = page_html.find(id="events-list-past")
-    past_events = past_events.find('div', 'althelete-total')
-    total_past_events = past_events.get_text()
-
-    # extract the total number of events as an int
-    count_pattern = '^\\d*'
-    total_past_events = int(re.search(count_pattern, total_past_events).group(0))
-
-    # extract the total number of event pages
-    total_pages = math.ceil(total_past_events / events_per_page)
-
-    return total_pages
+from parameters import headers, events_page
 
 
 def extract_event_ids(events_page=events_page, headers=headers):
@@ -68,19 +37,17 @@ def extract_event_ids(events_page=events_page, headers=headers):
     return event_ids
 
 
-def get_most_recent_event_ids(most_recent_event_id, events_page=events_page, headers=headers):
+def tmp_func(most_recent_event_id, events_page=events_page, headers=headers):
     """
     Inputs:
-        most_recent_event_id: str
-            the most recent event id in the fight database
         events_page: str
             url of the first page of past events
         headers: dict
             headers defines the user agent and is used in the get request
     Outputs:
-        new_rows: pd.DataFrame | None
+        event_ids_df: pd.DataFrame
             index:
-                2) event_id: int
+                1) event_id: int
             columns:
                 1) event_url: str
     """
@@ -96,26 +63,86 @@ def get_most_recent_event_ids(most_recent_event_id, events_page=events_page, hea
     event_ids_df = pd.DataFrame(event_ids, columns=['event'])
     event_ids_df = event_ids_df['event'].str.split(pat='#', expand=True)
     event_ids_df.columns = ['event_url', 'event_id']
+    event_ids_df['event_id'] = event_ids_df['event_id'].astype(int)
 
-    event_ids_df['event_id'] = pd.to_numeric(event_ids_df['event_id'])
-
-    # filter dataframe to only have new events
-    if event_ids_df.loc[0, 'event_id'] != most_recent_event_id:
-        most_recent_index = event_ids_df[event_ids_df['event_id'] == most_recent_event_id]\
-            .index\
-            .values\
-            .astype(int)[0]
-
-        new_rows = event_ids_df.loc[0:(most_recent_index - 1), :]\
-            .set_index('event_id', drop=True)
-        return new_rows
-    else:
+    # get the row index of the most recently occured event_id
+    idx = event_ids_df[event_ids_df['event_id'] == most_recent_event_id].index.values[0]
+    if idx == 0:
         return None
+    elif most_recent_event_id in event_ids_df['event_id'].tolist():
+        event_ids_df = event_ids_df.iloc[:idx, ]   
+        return event_ids_df
+    else:
+        pass
+        # NOTE: this is where the function needs to iterate until the most recent id has been matched
 
 
-# TO DO: need to add function which updates data when there are more than one
-# page of new eventids. Currently data is only being updated when the
-# new data is on the first page of past events on the ufc site.
+def get_all_event_ids(event_ids_df, total_pages):
+    """
+    Inputs:
+        event_ids_df: pd.DataFrame
+            index:
+                1) Default/Row Id
+            columns:
+                1) event_url: str
+                2) event_id: str
+        total_pages: int
+            integer count of the total past ufc events divided by events per page
+    """
+    # loop through each page and append the event url and id to the dataframe
+    list_tmp_dfs = []
+    events_page_format = 'https://www.ufc.com/events?page={}'
+    tmp_pattern = "(?<=href=\"/event/).*(?=\"><span)"
+
+    for page_num in range(1, total_pages):
+        # select the correct page from the listed past events
+        tmp_page = events_page_format.format(page_num)
+
+        raw_html = get(tmp_page, headers=headers)
+        html = BeautifulSoup(raw_html.content, 'html.parser')
+
+        tmp_ids = []
+
+        tmp_events_past = html.find(id="events-list-past")
+        tmp_result_actions = tmp_events_past.find_all(
+            'div',
+            'c-card-event--result__actions'
+            )
+
+        for i in list(range(len(tmp_result_actions))):
+            try:
+                tmp = tmp_result_actions[i].find('a')
+                tmp_ids.append(tmp)
+            except KeyError:
+                pass
+
+        tmp_ids = [re.findall(tmp_pattern, str(event)) for event in tmp_ids]
+
+        # flatten the list
+        tmp_ids = [item for sublist in tmp_ids for item in sublist]
+
+        tmp_df = pd.DataFrame(tmp_ids, columns=['event'])
+        tmp_df = tmp_df['event'].str.split(pat='#', expand=True)
+
+        tmp_df.columns = ['event_url', 'event_id']
+
+        list_tmp_dfs.append(tmp_df)
+
+    event_ids_df = pd.concat([event_ids_df, pd.concat(list_tmp_dfs)])
+    event_ids_df['event_id'] = pd.to_numeric(event_ids_df['event_id'])
+    event_ids_df.set_index('event_id', drop=True, inplace=True)
+
+    return event_ids_df
+
+
+# NOTE: The current issue I have found is that the event_id are not incremented linearly. For example
+# UFC 246 is event_id 963 but UFC 247 is event_id 958
+# This is a problem because I can't select the highest event_id from the db and use
+# it to compare to the newly scraped event_id. I need to improve SQL skills to just return the first row of the 
+# table without ordering the column. Then I need to return all of the scraped event_ids that occur before 
+# that event_id
+
+
 if __name__ == "__main__":
     conn = psycopg2.connect(
         host="localhost",
@@ -125,10 +152,10 @@ if __name__ == "__main__":
     )
 
     cur = conn.cursor()
-    cur.execute("SELECT event_id FROM eventid WHERE event_id IS NOT NULL ORDER BY event_id DESC;")
+    cur.execute("SELECT event_id FROM eventid WHERE event_id IS NOT NULL;")
 
-    most_recent_event_id = cur.fetchone()[0]
-    event_df_updated = get_most_recent_event_ids(most_recent_event_id)
+    most_recent_event_id = int(cur.fetchone()[0])
+    event_df_updated = tmp_func(most_recent_event_id)
 
     if event_df_updated is not None:
         print('Updating Database with new Event Data')
