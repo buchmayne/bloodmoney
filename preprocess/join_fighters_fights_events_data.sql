8a394b8e619a06ec312e7f0df24609d967da3dd5
buchmayne <michaelmbuchman@gmail.com>
2020-04-04 16:04:49 -0700

    updated query to write the output to a new table in the database

--- preprocess/join_fighters_fights_events_data.sql
+++ preprocess/join_fighters_fights_events_data.sql
@@ -1,19 +1,20 @@
+CREATE TABLE joined_fight_event_fighters_data AS
 SELECT 
-    event_data.event_id,
     event_data.date,
     event_data.country,
     event_data.city,
     event_data.cur_fight,
     fighters_data.*,
-    fights_data.fight_id,
     fights_data.accolade_name,
     fights_data.weight_class_id,
     fights_data.weight_class_name,
     fights_data.possible_rds,
     fights_data.method,
-    fights_data.ending_round_num,
-    fights_data.event_id
+    fights_data.ending_round_num
 FROM 
     event_data
-INNER JOIN fighters_data ON (event_data.event_id = fighters_data.event_id) 
-INNER JOIN fights_data ON (event_data.event_id = fights_data.event_id);
+INNER JOIN fighters_data 
+    ON (event_data.event_id = fighters_data.event_id) 
+INNER JOIN fights_data 
+    ON (event_data.event_id = fights_data.event_id)
+    AND (fighters_data.fight_id = fights_data.fight_id);
